6b203b1c68d8267f7f4a801525f7e9ddaa621619
yangpixi <2602340701@qq.com>
2025-12-27 22:37:00 +0800

    chore: 完成Theme基本测试

---
+++ composeApp/src/commonMain/kotlin/com/yangpixi/rememberdrinking/presentation/screen/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.yangpixi.rememberdrinking.presentation.screen
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * @author yangpixi
+ * @date 2025/12/27 22:34
+ * @description viewModel
+ */
+
+class HomeViewModel : ViewModel() {
+
+}
