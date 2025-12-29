55e259dedfefef1de4797643cc397e802dfd90dd
yangpixi <2602340701@qq.com>
2025-12-29 21:55:00 +0800

    chore: 完成转换器的配置

--- composeApp/src/commonMain/kotlin/com/yangpixi/rememberdrinking/di/CommonModule.kt
+++ composeApp/src/commonMain/kotlin/com/yangpixi/rememberdrinking/di/CommonModule.kt
@@ -1,8 +1,14 @@
 package com.yangpixi.rememberdrinking.di
 
+import app.cash.sqldelight.ColumnAdapter
+import app.cash.sqldelight.db.SqlDriver
+import com.yangpixi.rememberdrinking.db.Database
+import com.yangpixi.rememberdrinking.db.Water_intake_record
+import com.yangpixi.rememberdrinking.presentation.screen.HomeViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import org.koin.core.module.dsl.singleOf
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
@@ -16,4 +22,18 @@ val commonModule = module {
     single(named("ApplicationScope")) {
         CoroutineScope(SupervisorJob( ) + Dispatchers.Default)
     }
+
+    // home
+    singleOf(::HomeViewModel)
+
+    // 定义转换器
+    val booleanAdapter = object : ColumnAdapter<Boolean, Long> {
+        override fun decode(databaseValue: Long): Boolean = databaseValue == 1L
+        override fun encode(value: Boolean): Long = if (value) 1L else 0L
+    }
+    // 完成Database的注入
+    single {
+        Database(get<SqlDriver>(), Water_intake_record.Adapter(booleanAdapter))
+    }
+
 }
