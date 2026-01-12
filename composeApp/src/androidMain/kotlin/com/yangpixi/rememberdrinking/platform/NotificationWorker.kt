009ed9a3aa5d81e75b0cd6d8032133b2b3fe2c4e
yangpixi <2602340701@qq.com>
2026-01-12 19:53:08 +0800

    chore: 安卓端添加夜间勿扰模式

--- composeApp/src/androidMain/kotlin/com/yangpixi/rememberdrinking/platform/NotificationWorker.kt
+++ composeApp/src/androidMain/kotlin/com/yangpixi/rememberdrinking/platform/NotificationWorker.kt
@@ -1,6 +1,7 @@
 package com.yangpixi.rememberdrinking.platform
 
 import android.content.Context
+import android.icu.util.Calendar
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.work.Worker
@@ -18,6 +19,11 @@ class NotificationWorker(context: Context, params: WorkerParameters) : Worker(co
         val title = inputData.getString("title") ?: "Reminder"
         val content = inputData.getString("content") ?: "Remember to drink water!"
         val id = inputData.getInt("id", 0)
+
+        val currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+        if (currentHour !in 8..<23) {
+            return Result.success() // 夜间时间不进行提醒
+        }
 
         try {
             val builder = NotificationCompat.Builder(applicationContext, "drink_channel")
