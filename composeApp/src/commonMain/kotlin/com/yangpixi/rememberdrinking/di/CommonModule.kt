b8434223defa46932a565611911bd86b282e1173
yangpixi <2602340701@qq.com>
2026-01-14 22:10:01 +0800

    refactor: 更换软件图标

--- composeApp/src/commonMain/kotlin/com/yangpixi/rememberdrinking/di/CommonModule.kt
+++ composeApp/src/commonMain/kotlin/com/yangpixi/rememberdrinking/di/CommonModule.kt
@@ -4,12 +4,16 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import app.cash.sqldelight.db.SqlDriver
 import com.yangpixi.rememberdrinking.data.api.AuthApi
+import com.yangpixi.rememberdrinking.data.api.UserApi
+import com.yangpixi.rememberdrinking.data.repository.UserRepoImpl
 import com.yangpixi.rememberdrinking.data.repository.WaterRepo
 import com.yangpixi.rememberdrinking.db.Database
 import com.yangpixi.rememberdrinking.presentation.screen.auth.login.LoginViewModel
 import com.yangpixi.rememberdrinking.presentation.screen.auth.register.RegisterViewModel
 import com.yangpixi.rememberdrinking.presentation.screen.history.HistoryViewModel
 import com.yangpixi.rememberdrinking.presentation.screen.home.HomeViewModel
+import com.yangpixi.rememberdrinking.presentation.screen.profile.ProfileViewModel
+import com.yangpixi.rememberdrinking.presentation.screen.settings.SettingsViewModel
 import com.yangpixi.rememberdrinking.util.AuthManager
 import com.yangpixi.rememberdrinking.util.GlobalSnackBarUtils
 import com.yangpixi.rememberdrinking.util.getClient
@@ -18,6 +22,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
@@ -69,7 +74,18 @@ val commonModule = module {
     // 获取全局snackbar工具
     singleOf(::GlobalSnackBarUtils)
 
+    // 注册界面viewModel
     viewModel {
         RegisterViewModel(get(), get())
     }
+
+    // 获取userRepo相关
+    singleOf(::UserApi)
+    singleOf(::UserRepoImpl)
+
+    // 设置viewModel
+    viewModelOf(::SettingsViewModel)
+
+    // 资料界面Profile
+    viewModelOf(::ProfileViewModel)
 }
--- composeApp/src/commonMain/kotlin/com/yangpixi/rememberdrinking/presentation/screen/about/AboutScreen.kt
+++ composeApp/src/commonMain/kotlin/com/yangpixi/rememberdrinking/presentation/screen/about/AboutScreen.kt
@@ -8,14 +8,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import rememberdrinking.composeapp.generated.resources.Res
-import rememberdrinking.composeapp.generated.resources.app_logo
+import rememberdrinking.composeapp.generated.resources.drop
 
 /**
  * @author yangpixi
@@ -33,7 +33,7 @@ fun AboutScreen() {
         verticalArrangement = Arrangement.Center
     ) {
         Image(
-            painter = painterResource(Res.drawable.app_logo),
+            painter = painterResource(Res.drawable.drop),
             contentDescription = null,
             modifier = Modifier
                 .height(100.dp)
