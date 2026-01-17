854319182740224665b64c21286fa63b114312a3
yangpixi <2602340701@qq.com>
2026-01-17 15:11:02 +0800

    chore: profile界面添加退出登录按钮

--- composeApp/src/commonMain/kotlin/com/yangpixi/rememberdrinking/App.kt
+++ composeApp/src/commonMain/kotlin/com/yangpixi/rememberdrinking/App.kt
@@ -175,7 +175,7 @@ fun App() {
                 }
 
                 composable("profile") {
-                    ProfileScreen()
+                    ProfileScreen(navController = navController)
                 }
             }
         }
